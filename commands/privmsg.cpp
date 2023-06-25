97d94a8cc745eb6bdf4c2bfabe703b1f50cefc26
Jordan Sauvain <jsauvain@1D2.42angouleme.fr>
2023-06-25 14:55:15 +0200

    commands

--- commands/privmsg.cpp
+++ commands/privmsg.cpp
@@ -6,13 +6,13 @@
 /*   By: jsauvain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 08:52:23 by jsauvain          #+#    #+#             */
-/*   Updated: 2023/06/24 10:02:31 by jsauvain         ###   ########.fr       */
+/*   Updated: 2023/06/25 13:50:30 by jsauvain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 
-void	Server::_sendMessage(Client & client, int indexClient)
+void	Server::_sendMessage(Client & client, int indexDestClient)
 {
 		std::string	message;
 		size_t		pos = 0;
@@ -20,11 +20,13 @@ void	Server::_sendMessage(Client & client, int indexClient)
 		for (int i = 0; i < 2; i++)
 		{
 			pos = client.getInput().find_first_of(" 	", pos);
+			if (pos == std::string::npos)
+				return ;
 			pos = client.getInput().find_first_not_of(" 	", pos);
 		}
 		message = client.getInput().substr(pos);
 		//Mettre une longeur de message max ?
-		if (send(_allClients[indexClient].getSocket(), message.c_str(), \
+		if (send(_allClients[indexDestClient].getSocket(), message.c_str(), \
 			message.length(), MSG_DONTWAIT) == -1)
 			std::cout << "Message could not be sent\n";
 }
@@ -34,14 +36,24 @@ void	Server::_privmsg(std::stringstream & ss, Client & client)
 		std::vector<Client>::iterator	it;
 		std::string						nickname;
 
-		ss >> nickname;
-		for (it = _allClients.begin(); it < _allClients.end(); it++)
+		if (client.getNickname().empty() == true)
+			std::cout << "You have to set a nickname\n";
+		else if (client.getUsername().empty() == true)
+			std::cout << "You have to set a username\n";
+		else
 		{
-			if (it->getNickname().compare(nickname) == 0)
+			ss >> nickname;
+			for (it = _allClients.begin(); it < _allClients.end(); it++)
 			{
-				_sendMessage(client, it - _allClients.begin());
-				return ;
+				if (it->getNickname().compare(nickname) == 0)
+				{
+					_sendMessage(client, it - _allClients.begin());
+					return ;
+				}
 			}
+			if (nickname.empty() == false)
+				std::cout << "user '" << nickname << "' not found\n";
+			else
+				std::cout << "You must specify who the message is adressed to\n";
 		}
-		std::cout << nickname << " not found\n";
 }		
