441d007f05bf36d4b53266f2e6d1eda5fee069f8
Jordan Sauvain <jsauvain@3F2.42angouleme.fr>
2023-06-29 17:58:49 +0200

    authentification

--- detectCommand.cpp
+++ detectCommand.cpp
@@ -4,19 +4,34 @@ bool	Server::_authentification(std::string token, std::istringstream & ss, Clien
 {
 	bool	status = false;
 
-	if (token.compare("NICK") == 0)
+	if (token.compare("pass") == 0 || token.compare("PASS") == 0)
 	{
+		_pass(ss, client);
+		status = true;
+	}
+	else if (token.compare("NICK") == 0)
+	{
+		if (client.getPassword() == false)
+		{
+			_sendMessageToClient(client, HEX_INFO + " You must enter the correct password\n");
+			return (true);
+		}
 		_nick(ss, client);
 		status = true;
 	}
 	else if (token.compare("user") == 0 || token.compare("USER") == 0)
 	{
+		if (client.getPassword() == false)
+		{
+			_sendMessageToClient(client, HEX_INFO + " You must enter the correct password\n");
+			return (true);
+		}
+		else if (client.getNickname().first == false)
+		{
+			_sendMessageToClient(client, HEX_INFO + " You must set a nickname\n");
+			return (true);
+		}
 		_user(ss, client);
-		status = true;
-	}
-	else if (token.compare("pass") == 0 || token.compare("PASS") == 0)
-	{
-		_pass(ss, client);
 		status = true;
 	}
 	if (client.getUsername().second.empty() == false && client.getNickname().second.empty() == false
@@ -39,7 +54,8 @@ void	Server::_detectCommand(Client &client)
 	if (_authentification(token, ss, client) == true)
 		return ;
 	if (client.getAuthentification() == false)
-		_sendMessageToClient(client, HEX_INFO + " You must set a username, a nickname and give the correct password to be able to use commands\n");
+		_sendMessageToClient(client, HEX_INFO + " You must set a username, " +
+			"a nickname and give the correct password to be able to use commands\n");
 	else if (token.compare("PRIVMSG") == 0)
 		_privmsg(ss, client);
 	else if (token.compare("JOIN") == 0)
