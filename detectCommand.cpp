b98bea35becfa74a360e46965a55546cc63fcdee
Jordan Sauvain <jsauvain@3F2.42angouleme.fr>
2023-06-29 18:19:36 +0200

    channels messages + authentification

--- detectCommand.cpp
+++ detectCommand.cpp
@@ -1,5 +1,48 @@
 #include "Server.hpp"
 
+bool	Server::_authentification(std::string token, std::istringstream & ss, Client & client)
+{
+	bool	status = false;
+
+	if (token.compare("pass") == 0 || token.compare("PASS") == 0)
+	{
+		_pass(ss, client);
+		status = true;
+	}
+	else if (token.compare("NICK") == 0)
+	{
+		if (client.getPassword() == false)
+		{
+			_sendMessageToClient(client, HEX_INFO + " You must enter the correct password\n");
+			return (true);
+		}
+		_nick(ss, client);
+		status = true;
+	}
+	else if (token.compare("user") == 0 || token.compare("USER") == 0)
+	{
+		if (client.getPassword() == false)
+		{
+			_sendMessageToClient(client, HEX_INFO + " You must enter the correct password\n");
+			return (true);
+		}
+		else if (client.getNickname().first == false)
+		{
+			_sendMessageToClient(client, HEX_INFO + " You must set a nickname\n");
+			return (true);
+		}
+		_user(ss, client);
+		status = true;
+	}
+	if (client.getUsername().second.empty() == false && client.getNickname().second.empty() == false
+		&& client.getPassword() == true && client.getAuthentification() == false)
+	{
+		client.setAuthentification(true);
+		_sendMessageToClient(client, HEX_INFO + " You successfully authentificated\n");
+	}
+	return (status);
+}
+
 void	Server::_detectCommand(Client &client)
 {
 	std::istringstream	ss(client.getInput());
@@ -8,14 +51,13 @@ void	Server::_detectCommand(Client &client)
 	std::cout << "--------Detect command--------" <<std::endl;
 	ss >> token;
 	std::cout << client.getInput() << std::endl;
-	if (token.compare("NICK") == 0)
-		_nick(ss, client);
-	else if (token.compare("user") == 0 || token.compare("USER") == 0)
-		_user(ss, client);
-	else if (token.compare("pass") == 0 || token.compare("PASS") == 0)
-		_pass(ss, client);
-	else if (client.getNickname().first != true || client.getUsername().first != true || client.getPassword() != true)
-		_sendMessageToClient(client, HEX_INFO + " You must set a username, a nickname and give the correct password to be able to use commands\n");
+	if (_authentification(token, ss, client) == true)
+		return ;
+	if (client.getAuthentification() == false)
+		_sendMessageToClient(client, HEX_INFO + " You must set a username, " +
+			"a nickname and give the correct password to be able to use commands\n");
+	else if (token.compare("PRIVMSG") == 0)
+		_privmsg(ss, client);
 	else if (token.compare("JOIN") == 0)
 		_join(ss, client);
 	else if (token.compare("whois") == 0 || token.compare("WHOIS") == 0)
@@ -24,4 +66,9 @@ void	Server::_detectCommand(Client &client)
 		_displayChannels(client);
 	else if (token.compare("QUIT") == 0)
 		_disconnectClient(client);
+	else
+	{
+		token = "Unknown command " + token + "\n";
+		send(client.getSocket(), token.c_str(), token.length(), MSG_DONTWAIT);
+	}
 }
